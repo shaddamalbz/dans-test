a7e294497fec79c4f56e0350274d6e1f370b427e
shaddamalbz <shaddam.albz@gmail.com>
2023-04-08 22:14:24 +0700

    add login via facebook

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,17 @@ import { FC, useState, useEffect } from 'react'
 import { useGoogleLogin, TokenResponse } from '@react-oauth/google'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import FacebookLogin, { ProfileSuccessResponse } from '@greatsumini/react-facebook-login'
+import { FaFacebook } from 'react-icons/fa'
 
 import Dropdown from '@/components/Dropdown'
 import { Profile } from '@/types/'
+import LogoGoogle from '@/assets/svg/logoGoogle.svg'
 
 const Header: FC = () => {
   const [user, setUser] = useState<TokenResponse>()
-  const [profile, setProfile] = useState<Profile>()
+  const [profile, setProfile] = useState<any>()
+  const [loginWith, setLoginWith] = useState<'google' | 'facebook'>()
 
   const login = useGoogleLogin({
     onSuccess: (res) => setUser(res),
@@ -17,13 +21,14 @@ const Header: FC = () => {
   useEffect(() => {
     if (user) {
       axios
-        .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+        .get<Profile>(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
           headers: {
             Authorization: `Bearer ${user.access_token}`,
             Accept: 'application/json',
           },
         })
         .then((res) => {
+          setLoginWith('google')
           setProfile(res.data)
         })
     }
@@ -39,11 +44,21 @@ const Header: FC = () => {
         {profile ? (
           <Dropdown
             className="text-black"
-            triggerButton={<img className="h-10 rounded-full" src={profile.picture} alt="user image" />}
+            triggerButton={
+              <img
+                className="h-10 rounded-full"
+                src={loginWith === 'google' ? profile.picture : profile?.picture.data.url}
+                alt="user image"
+              />
+            }
           >
             <div>
               <div className="flex gap-x-2 items-center p-4">
-                <img className="h-10 rounded-full" src={profile.picture} alt="user image" />
+                <img
+                  className="h-10 rounded-full"
+                  src={loginWith === 'google' ? profile.picture : profile?.picture.data.url}
+                  alt="user image"
+                />
                 <div>
                   <div>{profile.name}</div>
                   <div className="text-xs">{profile.email}</div>
@@ -52,7 +67,32 @@ const Header: FC = () => {
             </div>
           </Dropdown>
         ) : (
-          <button onClick={() => login()}>Sign in with Google 🚀 </button>
+          <Dropdown triggerButton={<button>Login</button>}>
+            <div className="p-4 space-y-4">
+              <button
+                className="w-full flex gap-x-1 bg-white text-black text-opacity-50 font-bold rounded-lg shadow p-2 hover:bg-gray-100"
+                onClick={() => login()}
+              >
+                <img src={LogoGoogle} alt="Google" /> Continue with Google
+              </button>
+              <FacebookLogin
+                appId="5702631229838825"
+                onProfileSuccess={(response) => {
+                  setLoginWith('facebook')
+                  setProfile(response)
+                }}
+                render={({ onClick }) => (
+                  <button
+                    className="bg-[#1877F2] text-white w-full flex items-center gap-x-1 rounded-lg shadow p-2"
+                    onClick={onClick}
+                  >
+                    <FaFacebook size={24} />
+                    Continue with Facebook
+                  </button>
+                )}
+              />
+            </div>
+          </Dropdown>
         )}
       </div>
     </header>
